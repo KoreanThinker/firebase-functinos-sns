05a56f9deb7014f23dff750503bd308bf1e55184
hyun <coderhyun476@gmail.com>
2020-01-10 23:24:54 +0900

    백엔드 완료

---
+++ README.md
@@ -0,0 +1,9 @@
+# firebase-functions-sns
+rn-firebase-sns의 백엔드
+
+## 프론트엔드
+- [프론트엔드바로가기](https://github.com/KoreanThinker/rn-firebase-sns)
+
+## 참고
+- [로컬에서 serve할때 안되면 환경변수 지정해주자](https://firebase.google.com/docs/admin/setup?hl=ko)
+- [ctrl+shift+b -> watch tsc 로 hot load](https://firebase.googleblog.com/2018/01/streamline-typescript-development-cloud-functions.html)
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,10 +2,81 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
 admin.initializeApp();
+const db = admin.firestore();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const getPostList = functions.https.onCall(async data => {
+    try {
+        const snapshot: any = await db.collection('post').get()
+        const result: any = []
+        snapshot.forEach((doc: any) => {
+            result.push({ ...doc.data(), id: doc.id });
+        });
+
+        return {
+            post: result
+        }
+    } catch (error) {
+        throw new functions.https.HttpsError('failed-precondition', error);
+    }
+})
+
+export const getComment = functions.https.onCall(async id => {
+    try {
+        const snapshot: any = await db.collection('post').doc(id).get()
+        return snapshot.data().comment
+
+    } catch (error) {
+        throw new functions.https.HttpsError('failed-precondition', error);
+    }
+})
+
+export const deletePost = functions.https.onCall(async id => {
+    try {
+        const deleteDoc: any = await db.collection('post').doc(id).delete()
+        console.log(deleteDoc);
+        return true;
+    } catch (error) {
+        throw new functions.https.HttpsError('failed-precondition', error);
+    }
+})
+
+export const writePost = functions.https.onCall(async data => {
+    try {
+        const ref: any = await db.collection('post').add({ ...data, likeCount: 0 });
+        return ref.id
+    } catch (error) {
+        throw new functions.https.HttpsError('failed-precondition', error);
+    }
+})
+
+export const likePost = functions.https.onCall(async id => {
+    try {
+        console.log(id);
+        const post: any = await db.collection('post').doc(id).get()
+        await db.collection('post').doc(id).update({ likeCount: post.data().likeCount + 1 })
+        return true
+    } catch (error) {
+        throw new functions.https.HttpsError('failed-precondition', error);
+    }
+})
+
+export const commentPost = functions.https.onCall(async data => {
+    try {
+
+    } catch (error) {
+        throw new functions.https.HttpsError('failed-precondition', error);
+    }
+})
+
+
+
+export const onValueChange = functions.firestore.document('test/hello').onUpdate(async change => {
+    const after: any = change.after.data()
+    const payload = {
+        data: {
+            info: String(after.info)
+        }
+    }
+    return admin.messaging().sendToTopic('testUpdate', payload);
+})
+
